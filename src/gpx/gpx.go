ae5dad6ecdf673dd303e21a01fa40dff1623a8f3
Tomo Krajina <tkrajina@gmail.com>
2014-07-28 22:49:22 +0200

    Moved gpx11/gpx10 converters to converters.go

--- src/gpx/gpx.go
+++ src/gpx/gpx.go
@@ -3,7 +3,7 @@ package gpx
 import (
 	"encoding/xml"
 	"errors"
-	"fmt"
+	//	"fmt"
 	"gpx11"
 	"io/ioutil"
 	"os"
@@ -46,52 +46,7 @@ func (g *GPX) ToXml(version string) ([]byte, error) {
 	if version == "1.0" {
 		return nil, errors.New("Invalid version:" + version)
 	} else if version == "1.1" {
-		gpx11Doc := gpx11.NewGpx()
-
-		gpx11Doc.Creator = g.Creator
-		gpx11Doc.Version = g.Version
-		gpx11Doc.Name = g.Name
-		gpx11Doc.Desc = g.Description
-		gpx11Doc.AuthorName = g.AuthorName
-
-		if len(g.AuthorEmail) > 0 {
-			parts := strings.Split(g.AuthorEmail, "@")
-			if len(parts) == 1 {
-				gpx11Doc.AuthorEmail = new(gpx11.GpxEmail)
-				gpx11Doc.AuthorEmail.Id = parts[0]
-			} else if len(parts) > 1 {
-				gpx11Doc.AuthorEmail = new(gpx11.GpxEmail)
-				gpx11Doc.AuthorEmail.Id = parts[0]
-				gpx11Doc.AuthorEmail.Domain = parts[1]
-			}
-		}
-
-		if len(g.AuthorLink) > 0 || len(g.AuthorLinkText) > 0 || len(g.AuthorLinkType) > 0 {
-			gpx11Doc.AuthorLink = new(gpx11.GpxLink)
-			gpx11Doc.AuthorLink.Href = g.AuthorLink
-			gpx11Doc.AuthorLink.Text = g.AuthorLinkText
-			gpx11Doc.AuthorLink.Type = g.AuthorLinkType
-		}
-
-		if len(g.Copyright) > 0 || len(g.CopyrightYear) > 0 || len(g.CopyrightLicense) > 0 {
-			gpx11Doc.Copyright = new(gpx11.GpxCopyright)
-			gpx11Doc.Copyright.Author = g.Copyright
-			gpx11Doc.Copyright.Year = g.CopyrightYear
-			gpx11Doc.Copyright.License = g.CopyrightLicense
-		}
-
-		if len(g.Link) > 0 || len(g.LinkText) > 0 || len(g.LinkType) > 0 {
-			gpx11Doc.Link = new(gpx11.GpxLink)
-			gpx11Doc.Link.Href = g.Link
-			gpx11Doc.Link.Text = g.LinkText
-			gpx11Doc.Link.Type = g.LinkType
-		}
-
-		if g.Time != nil {
-			gpx11Doc.Timestamp = formatGPXTime(g.Time)
-		}
-
-		gpx11Doc.Keywords = g.Keywords
+		gpx11Doc := convertToGpx11Models(g)
 
 		return xml.Marshal(gpx11Doc)
 	} else {
@@ -145,7 +100,6 @@ func ParseFile(fileName string) (*GPX, error) {
 
 func ParseString(bytes []byte) (*GPX, error) {
 	version := guessGPXVersion(bytes)
-	result := new(GPX)
 	if version == "1.0" {
 		return nil, errors.New("Invalid version:" + version)
 	} else if version == "1.1" {
@@ -155,45 +109,7 @@ func ParseString(bytes []byte) (*GPX, error) {
 			return nil, err
 		}
 
-		result.Creator = g.Creator
-		result.Version = g.Version
-		result.Name = g.Name
-		result.Description = g.Desc
-		result.AuthorName = g.AuthorName
-
-		if g.AuthorEmail != nil {
-			result.AuthorEmail = g.AuthorEmail.Id + "@" + g.AuthorEmail.Domain
-		}
-		if g.AuthorLink != nil {
-			result.AuthorLink = g.AuthorLink.Href
-			result.AuthorLinkText = g.AuthorLink.Text
-			result.AuthorLinkType = g.AuthorLink.Type
-		}
-
-		if g.Extensions != nil {
-			result.Extensions = &g.Extensions.Bytes
-		}
-
-		fmt.Println("ts=", g.Timestamp)
-		if len(g.Timestamp) > 0 {
-			result.Time, _ = parseGPXTime(g.Timestamp)
-		}
-
-		if g.Copyright != nil {
-			result.Copyright = g.Copyright.Author
-			result.CopyrightYear = g.Copyright.Year
-			result.CopyrightLicense = g.Copyright.License
-		}
-
-		if g.Link != nil {
-			result.Link = g.Link.Href
-			result.LinkText = g.Link.Text
-			result.LinkType = g.Link.Type
-		}
-
-		result.Keywords = g.Keywords
-
-		return result, nil
+		return convertFromGpx11Models(g), nil
 	} else {
 		return nil, errors.New("Invalid version:" + version)
 	}
