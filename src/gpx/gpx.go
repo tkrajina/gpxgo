070288c752ac6a7028dff26975f582efb49b4efd
Tomo Krajina <tkrajina@gmail.com>
2014-07-28 06:13:58 +0200

    gotmft

--- src/gpx/gpx.go
+++ src/gpx/gpx.go
@@ -1,152 +1,152 @@
 package gpx
 
 import (
-    "os"
-    "io/ioutil"
-    "errors"
-    "gpx11"
-    "encoding/xml"
-    "strings"
-    "fmt"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"gpx11"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 type GPX struct {
-    Version string
-    Creator string
-    Name string
-    Description string
-    AuthorName string
-    AuthorEmail string
-    AuthorLink string
-    AuthorLinkText string
-    AuthorLinkType string
-    Copyright string
-    CopyrightYear string
-    CopyrightLicense string
-    Link string
-    LinkText string
-    LinkType string
+	Version          string
+	Creator          string
+	Name             string
+	Description      string
+	AuthorName       string
+	AuthorEmail      string
+	AuthorLink       string
+	AuthorLinkText   string
+	AuthorLinkType   string
+	Copyright        string
+	CopyrightYear    string
+	CopyrightLicense string
+	Link             string
+	LinkText         string
+	LinkType         string
 
-    // TODO
-    Extensions *[]byte
+	// TODO
+	Extensions *[]byte
 }
 
 func (g *GPX) ToXml(version string) ([]byte, error) {
-    if version == "1.0" {
-        return nil, errors.New("Invalid version:" + version)
-    } else if version == "1.1" {
-        gpx11Doc := gpx11.NewGpx()
+	if version == "1.0" {
+		return nil, errors.New("Invalid version:" + version)
+	} else if version == "1.1" {
+		gpx11Doc := gpx11.NewGpx()
 
-        gpx11Doc.Creator = g.Creator
-        gpx11Doc.Version = g.Version
-        gpx11Doc.Name = g.Name
-        gpx11Doc.Desc = g.Description
-        gpx11Doc.AuthorName = g.AuthorName
+		gpx11Doc.Creator = g.Creator
+		gpx11Doc.Version = g.Version
+		gpx11Doc.Name = g.Name
+		gpx11Doc.Desc = g.Description
+		gpx11Doc.AuthorName = g.AuthorName
 
-        if len(g.AuthorEmail) > 0 {
-            parts := strings.Split(g.AuthorEmail, "@")
-            if len(parts) == 1 {
-                gpx11Doc.AuthorEmail = new(gpx11.GpxEmail)
-                gpx11Doc.AuthorEmail.Id = parts[0]
-            } else if len(parts) > 1 {
-                gpx11Doc.AuthorEmail = new(gpx11.GpxEmail)
-                gpx11Doc.AuthorEmail.Id = parts[0]
-                gpx11Doc.AuthorEmail.Domain = parts[1]
-            }
-        }
+		if len(g.AuthorEmail) > 0 {
+			parts := strings.Split(g.AuthorEmail, "@")
+			if len(parts) == 1 {
+				gpx11Doc.AuthorEmail = new(gpx11.GpxEmail)
+				gpx11Doc.AuthorEmail.Id = parts[0]
+			} else if len(parts) > 1 {
+				gpx11Doc.AuthorEmail = new(gpx11.GpxEmail)
+				gpx11Doc.AuthorEmail.Id = parts[0]
+				gpx11Doc.AuthorEmail.Domain = parts[1]
+			}
+		}
 
-        if len(g.AuthorLink) > 0 || len(g.AuthorLinkText) > 0 || len(g.AuthorLinkType) > 0 {
-            gpx11Doc.AuthorLink = new(gpx11.GpxLink)
-            gpx11Doc.AuthorLink.Href = g.AuthorLink
-            gpx11Doc.AuthorLink.Text = g.AuthorLinkText
-            gpx11Doc.AuthorLink.Type = g.AuthorLinkType
-        }
+		if len(g.AuthorLink) > 0 || len(g.AuthorLinkText) > 0 || len(g.AuthorLinkType) > 0 {
+			gpx11Doc.AuthorLink = new(gpx11.GpxLink)
+			gpx11Doc.AuthorLink.Href = g.AuthorLink
+			gpx11Doc.AuthorLink.Text = g.AuthorLinkText
+			gpx11Doc.AuthorLink.Type = g.AuthorLinkType
+		}
 
-        if len(g.Copyright) > 0 || len(g.CopyrightYear) > 0 || len(g.CopyrightLicense) > 0 {
-            gpx11Doc.Copyright = new(gpx11.GpxCopyright)
-            gpx11Doc.Copyright.Author = g.Copyright
-            gpx11Doc.Copyright.Year = g.CopyrightYear
-            gpx11Doc.Copyright.License = g.CopyrightLicense
-        }
+		if len(g.Copyright) > 0 || len(g.CopyrightYear) > 0 || len(g.CopyrightLicense) > 0 {
+			gpx11Doc.Copyright = new(gpx11.GpxCopyright)
+			gpx11Doc.Copyright.Author = g.Copyright
+			gpx11Doc.Copyright.Year = g.CopyrightYear
+			gpx11Doc.Copyright.License = g.CopyrightLicense
+		}
 
-        if len(g.Link) > 0 || len(g.LinkText) > 0 || len(g.LinkType) > 0 {
-            gpx11Doc.Link = new(gpx11.GpxLink)
-            gpx11Doc.Link.Href = g.Link
-            gpx11Doc.Link.Text = g.LinkText
-            gpx11Doc.Link.Type = g.LinkType
-        }
+		if len(g.Link) > 0 || len(g.LinkText) > 0 || len(g.LinkType) > 0 {
+			gpx11Doc.Link = new(gpx11.GpxLink)
+			gpx11Doc.Link.Href = g.Link
+			gpx11Doc.Link.Text = g.LinkText
+			gpx11Doc.Link.Type = g.LinkType
+		}
 
-        return xml.Marshal(gpx11Doc)
-    } else {
-        return nil, errors.New("Invalid version " + version)
-    }
+		return xml.Marshal(gpx11Doc)
+	} else {
+		return nil, errors.New("Invalid version " + version)
+	}
 }
 
-func guessGPXVersion(bytes []byte) (string) {
-    return "1.1"
+func guessGPXVersion(bytes []byte) string {
+	return "1.1"
 }
 
 func ParseFile(fileName string) (*GPX, error) {
-    f, err := os.Open(fileName)
-    if err != nil {
-        return nil, err
-    }
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
 
-    bytes, err := ioutil.ReadAll(f)
-    if err != nil {
-        return nil, err
-    }
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
 
-    return ParseString(bytes)
+	return ParseString(bytes)
 }
 
 func ParseString(bytes []byte) (*GPX, error) {
-    version := guessGPXVersion(bytes)
-    result := new(GPX)
-    if version == "1.0" {
-        return nil, errors.New("Invalid version:" + version)
-    } else if version == "1.1" {
-        g := gpx11.NewGpx()
-        err := xml.Unmarshal(bytes, &g)
-        if err != nil {
-            return nil, err
-        }
+	version := guessGPXVersion(bytes)
+	result := new(GPX)
+	if version == "1.0" {
+		return nil, errors.New("Invalid version:" + version)
+	} else if version == "1.1" {
+		g := gpx11.NewGpx()
+		err := xml.Unmarshal(bytes, &g)
+		if err != nil {
+			return nil, err
+		}
 
-        result.Creator = g.Creator
-        result.Version = g.Version
-        result.Name = g.Name
-        result.Description = g.Desc
-        result.AuthorName = g.AuthorName
+		result.Creator = g.Creator
+		result.Version = g.Version
+		result.Name = g.Name
+		result.Description = g.Desc
+		result.AuthorName = g.AuthorName
 
-        if g.AuthorEmail != nil {
-            result.AuthorEmail = g.AuthorEmail.Id + "@" + g.AuthorEmail.Domain
-        }
-        if g.AuthorLink != nil {
-            result.AuthorLink = g.AuthorLink.Href
-            result.AuthorLinkText = g.AuthorLink.Text
-            result.AuthorLinkType = g.AuthorLink.Type
-        }
+		if g.AuthorEmail != nil {
+			result.AuthorEmail = g.AuthorEmail.Id + "@" + g.AuthorEmail.Domain
+		}
+		if g.AuthorLink != nil {
+			result.AuthorLink = g.AuthorLink.Href
+			result.AuthorLinkText = g.AuthorLink.Text
+			result.AuthorLinkType = g.AuthorLink.Type
+		}
 
-        if g.Extensions != nil {
-            result.Extensions = &g.Extensions.Bytes
-        }
+		if g.Extensions != nil {
+			result.Extensions = &g.Extensions.Bytes
+		}
 
-        fmt.Println("copyright", g.Copyright)
-        if g.Copyright != nil {
-            result.Copyright = g.Copyright.Author
-            result.CopyrightYear = g.Copyright.Year
-            result.CopyrightLicense = g.Copyright.License
-        }
+		fmt.Println("copyright", g.Copyright)
+		if g.Copyright != nil {
+			result.Copyright = g.Copyright.Author
+			result.CopyrightYear = g.Copyright.Year
+			result.CopyrightLicense = g.Copyright.License
+		}
 
-        if g.Link != nil {
-            result.Link = g.Link.Href
-            result.LinkText = g.Link.Text
-            result.LinkType = g.Link.Type
-        }
+		if g.Link != nil {
+			result.Link = g.Link.Href
+			result.LinkText = g.Link.Text
+			result.LinkType = g.Link.Type
+		}
 
-        return result, nil
-    } else {
-        fmt.Println("error")
-        return nil, errors.New("Invalid version:" + version)
-    }
+		return result, nil
+	} else {
+		fmt.Println("error")
+		return nil, errors.New("Invalid version:" + version)
+	}
 }
--- src/gpx/gpx_test.go
+++ src/gpx/gpx_test.go
@@ -1,63 +1,63 @@
 package gpx
 
 import (
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
 
 func assertEquals(t *testing.T, var1 interface{}, var2 interface{}) {
-    if var1 != var2 {
-        fmt.Println(var1, "not equals to", var2)
-        t.Error("Not equals")
-    }
+	if var1 != var2 {
+		fmt.Println(var1, "not equals to", var2)
+		t.Error("Not equals")
+	}
 }
 
 func TestParseAndReparseGPX11(t *testing.T) {
-    gpxDocuments := []*GPX {}
+	gpxDocuments := []*GPX{}
 
-    {
-        gpxDoc, err := ParseFile("../../test_files/gpx1.1_with_all_fields.gpx")
-        if err != nil || gpxDoc == nil {
-            t.Error("Error parsing:" + err.Error())
-        }
-        gpxDocuments = append(gpxDocuments, gpxDoc)
+	{
+		gpxDoc, err := ParseFile("../../test_files/gpx1.1_with_all_fields.gpx")
+		if err != nil || gpxDoc == nil {
+			t.Error("Error parsing:" + err.Error())
+		}
+		gpxDocuments = append(gpxDocuments, gpxDoc)
 
-        // Test after reparsing
-        xml, err := gpxDoc.ToXml("1.1")
-        fmt.Println(string(xml))
-        if err != nil {
-            t.Error("Error serializing to XML:" + err.Error())
-        }
-        gpxDoc2, err := ParseString(xml)
-        if err != nil {
-            t.Error("Error parsing XML:" + err.Error())
-        }
-        gpxDocuments = append(gpxDocuments, gpxDoc2)
+		// Test after reparsing
+		xml, err := gpxDoc.ToXml("1.1")
+		fmt.Println(string(xml))
+		if err != nil {
+			t.Error("Error serializing to XML:" + err.Error())
+		}
+		gpxDoc2, err := ParseString(xml)
+		if err != nil {
+			t.Error("Error parsing XML:" + err.Error())
+		}
+		gpxDocuments = append(gpxDocuments, gpxDoc2)
 
-        // TODO: ToString 1.0 and check again
-    }
+		// TODO: ToString 1.0 and check again
+	}
 
-    for i := 1; i < len(gpxDocuments); i++ {
-        fmt.Println("Testing gpx doc #", i)
+	for i := 1; i < len(gpxDocuments); i++ {
+		fmt.Println("Testing gpx doc #", i)
 
-        gpxDoc := gpxDocuments[i]
+		gpxDoc := gpxDocuments[i]
 
-        assertEquals(t, gpxDoc.Version, "1.1")
-        assertEquals(t, gpxDoc.Creator, "...")
-        assertEquals(t, gpxDoc.Name, "example name")
-        assertEquals(t, gpxDoc.AuthorName, "author name")
-        assertEquals(t, gpxDoc.AuthorEmail, "aaa@bbb.com")
-        assertEquals(t, gpxDoc.Description, "example description")
-        assertEquals(t, gpxDoc.AuthorLink, "http://link")
-        assertEquals(t, gpxDoc.AuthorLinkText, "link text")
-        assertEquals(t, gpxDoc.AuthorLinkType, "link type")
-        assertEquals(t, gpxDoc.Copyright, "gpxauth")
-        assertEquals(t, gpxDoc.CopyrightYear, "2013")
-        assertEquals(t, gpxDoc.CopyrightLicense, "lic")
-        assertEquals(t, gpxDoc.Link, "http://link2")
-        assertEquals(t, gpxDoc.LinkText, "link text2")
-        assertEquals(t, gpxDoc.LinkType, "link type2")
+		assertEquals(t, gpxDoc.Version, "1.1")
+		assertEquals(t, gpxDoc.Creator, "...")
+		assertEquals(t, gpxDoc.Name, "example name")
+		assertEquals(t, gpxDoc.AuthorName, "author name")
+		assertEquals(t, gpxDoc.AuthorEmail, "aaa@bbb.com")
+		assertEquals(t, gpxDoc.Description, "example description")
+		assertEquals(t, gpxDoc.AuthorLink, "http://link")
+		assertEquals(t, gpxDoc.AuthorLinkText, "link text")
+		assertEquals(t, gpxDoc.AuthorLinkType, "link type")
+		assertEquals(t, gpxDoc.Copyright, "gpxauth")
+		assertEquals(t, gpxDoc.CopyrightYear, "2013")
+		assertEquals(t, gpxDoc.CopyrightLicense, "lic")
+		assertEquals(t, gpxDoc.Link, "http://link2")
+		assertEquals(t, gpxDoc.LinkText, "link text2")
+		assertEquals(t, gpxDoc.LinkType, "link type2")
 
-        //assertEquals(t, gpxDoc.AuthorName, "author name")
-    }
+		//assertEquals(t, gpxDoc.AuthorName, "author name")
+	}
 }
